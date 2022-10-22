0de96bb3df9f27d33d5bd1f8267ab7051abac6c4
Kevin Lin <kevinl@cs.uw.edu>
2022-10-22 12:12:32 -0700

    test: Add TreeSetAutocompleteTests

---
+++ test/autocomplete/cities/TreeSetAutocompleteTests.java
@@ -0,0 +1,26 @@
+package autocomplete.cities;
+
+import autocomplete.Autocomplete;
+import autocomplete.TreeSetAutocomplete;
+
+/**
+ * Tests for the {@link TreeSetAutocomplete} class.
+ *
+ * @see TreeSetAutocomplete
+ */
+public class TreeSetAutocompleteTests extends AutocompleteTests {
+    @Override
+    public Autocomplete createAutocomplete() {
+        return new TreeSetAutocomplete();
+    }
+
+    @Override
+    void comparePrefixSea() {
+        // Disable this inherited test for the reference implementation
+    }
+
+    @Override
+    void compareRandomPrefixes() {
+        // Disable this inherited test for the reference implementation
+    }
+}
