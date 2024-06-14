9fe7dd3621b2919cd9913666e8d2c8ab61b51efc
Rohan Nagavarapu Pujare <rpujare@uw.edu>
2024-06-14 12:19:56 -0700

    test: Added bandaid-detecting DequeTests

--- test/deques/DequeTests.java
+++ test/deques/DequeTests.java
@@ -252,6 +252,52 @@ public abstract class DequeTests {
         assertEquals(0, actual);
     }
 
+    @Test
+    void bandaidTest(){
+        Deque<Object> deque = createDeque();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addFirst(4);
+        deque.addFirst(5);
+        deque.addFirst(6);
+        deque.addFirst(7);
+        deque.addFirst(8);
+        deque.addFirst(9);
+        deque.addFirst(10);
+        deque.addLast(11);
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeFirst();
+        assertEquals(9, deque.removeFirst());
+    }
+
+    @Test
+    void bandaidTest2(){
+        Deque<Object> deque = createDeque();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addFirst(4);
+        deque.addFirst(5);
+        deque.addFirst(6);
+        deque.addFirst(7);
+        deque.addFirst(8);
+        deque.addLast(9);
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        assertEquals(5, deque.removeFirst());
+    }
+
     @Nested
     class RuntimeExperiments {
         /**
