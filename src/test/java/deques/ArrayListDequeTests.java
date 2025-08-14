479d13c6ddd5bffe55791f5ab2a5845c256643b3
Kevin Lin <kevinl@cs.uw.edu>
2025-08-13 18:32:58 -0700

    src: Add browserHistoryRuntimeSimulation test

--- src/test/java/deques/ArrayListDequeTests.java
+++ src/test/java/deques/ArrayListDequeTests.java
@@ -1,5 +1,7 @@
 package deques;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Tests for the {@link ArrayListDeque} class.
  */
@@ -7,5 +9,30 @@ public class ArrayListDequeTests extends DequeTests {
     @Override
     public <E> Deque<E> createDeque() {
         return new ArrayListDeque<>();
+    }
+
+    @Test
+    void browserHistoryRuntimeSimulation() {
+        Deque<String> deque = createDeque();
+
+        // Simulate visiting uw.edu many times
+        for (int i = 0; i < 100000; i += 1) {
+            deque.addLast("uw.edu");
+        }
+
+        // Simulate removing the last hour of history
+        for (int i = 0; i < 100; i += 1) {
+            deque.removeLast();
+        }
+
+        // Simulate visiting uw.edu many more times
+        for (int i = 0; i < 1000000; i += 1) {
+            deque.addLast("uw.edu");
+        }
+
+        // Simulate removing history from over 3 months ago
+        for (int i = 0; i < 100000; i += 1) {
+            deque.removeFirst();
+        }
     }
 }
