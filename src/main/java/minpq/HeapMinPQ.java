8ed13111e6ede0b4e5ee403944dbeea38c321b0d
Kevin Lin <kevinl@cs.uw.edu>
2026-01-03 14:08:40 -0800

    src: Add MinPQ toString methods for debugging

--- src/main/java/minpq/HeapMinPQ.java
+++ src/main/java/minpq/HeapMinPQ.java
@@ -89,4 +89,9 @@ public class HeapMinPQ<E> implements MinPQ<E> {
         // TODO: Replace with your code
         throw new UnsupportedOperationException("Not implemented yet");
     }
+
+    @Override
+    public String toString() {
+        return pq.toString();
+    }
 }
